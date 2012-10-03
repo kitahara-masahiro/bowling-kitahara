9102c272fc04efa721bdceb4ec9fb18827ea2158
kitahara_masahiro <kitahara_masahiro@nexway.co.jp>
2012-10-03 14:08:14 +0900

    ボーリングゲームのスコア計算プログラム（フレーム数を計算できるようにした）

--- game.rb
+++ game.rb
@@ -5,21 +5,48 @@ class Game
 
   def initialize
     @score = 0
-    @throw_count = 0
+    @current_frame_score = 0
+    @current_frame_throw_count = 0
+    @total_count = 0
+
+    @frame = 1
   end
 
   def throw(point)
-    @throw_count += 1
-    if @throw_count <= 2
+    @total_count += 1
+    @current_frame_throw_count += 1
+    @current_frame_score += point
+
+    if @total_count <= 2
       @score += point
-    elsif @throw_count == 3 && @score == 10
+    elsif @total_count == 3 && @score == 10
       @score += point * 2
     end
+
+    if @current_frame_throw_count == 2 || strike?
+      @frame += 1
+      @current_frame_throw_count = 0
+      @current_frame_score = 0
+    end
+  end
+
+  def current_frame
+    @frame
   end
 
   def score()
     @score
   end
+
+  def strike?
+    @current_frame_throw_count == 1 && @current_frame_score == 10
+  end
+
+  def spare?
+    @current_frame_throw_count == 2 && @current_frame_score == 10
+  end
+
+
 end
 
 
@@ -52,10 +79,18 @@ unless __FILE__ == $0
       game.score.should == 10
     end
 
+    it '1回目ガーターだったら現在のフレームが1になること' do
+      game = Game.new
+      game.throw 0
+      game.current_frame.should == 1
+    end
+
+    it '1回目も2回目もガーターだったら現在のフレームが2になること' do
+      game = Game.new
+      game.throw 0
+      game.throw 0
+      game.current_frame.should == 2
+    end
+
   end
 end
-
-game = Game.new
-game.throw(1)
-game.throw(2)
-puts game.score()
